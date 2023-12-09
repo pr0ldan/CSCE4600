cbfef9343335829cfa4cab68208f5572643f484e
pr0ldan <paulina699@icloud.com>
2023-12-09 17:54:33 -0600

    Update let.go

--- Project2/builtins/let.go
+++ Project2/builtins/let.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"strconv"
+	"strings"
 )
 
 // Let evaluates arithmetic expressions and prints the result.
@@ -13,7 +14,11 @@ func Let(w io.Writer, args ...string) error {
 		return nil
 	}
 
-	result, err := evaluateExpressions(args...)
+	// Concatenate the expressions into a single string.
+	combinedExpr := strings.Join(args, " ")
+
+	// Evaluate the combined expression using the strconv package.
+	result, err := strconv.Atoi(combinedExpr)
 	if err != nil {
 		fmt.Fprintf(w, "Error: %s\n", err)
 		return err
@@ -21,20 +26,4 @@ func Let(w io.Writer, args ...string) error {
 
 	fmt.Fprintf(w, "%d\n", result)
 	return nil
-}
-
-func evaluateExpressions(expressions ...string) (int, error) {
-	// Concatenate the expressions into a single string.
-	combinedExpr := ""
-	for _, expr := range expressions {
-		combinedExpr += expr + " "
-	}
-
-	// Evaluate the combined expression using the strconv package.
-	result, err := strconv.Atoi(combinedExpr)
-	if err != nil {
-		return 0, fmt.Errorf("invalid expression: %s", combinedExpr)
-	}
-
-	return result, nil
 }
